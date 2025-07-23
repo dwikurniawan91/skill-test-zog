b1a4aa1aea4a29a05bf0f06ec05a230305de16d2
dwi.kur <dwi.kurniawan@youtap.id>
2025-07-23 10:44:15 +0700

    fix: fix unspecified __dirname

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,9 @@
-import path from "path";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 import { defineConfig } from "vitest/config";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig(() =>
 	defineConfig({
